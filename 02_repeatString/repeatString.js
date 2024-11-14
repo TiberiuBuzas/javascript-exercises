fb268f05f9461b741d7e88cf8e70dfd8331af38c
Tiberiu Buzas <48926984+TiberiuBuzas@users.noreply.github.com>
2024-11-14 22:24:56 +0100

    Completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(pString, pNum) {
+    if(pNum >= 0){
+        let string = "";
 
+        for (let index = 0; index < pNum; index++) {
+            string += pString;
+        }
+
+        return string;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
