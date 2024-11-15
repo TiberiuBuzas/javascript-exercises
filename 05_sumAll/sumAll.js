ea86d3a874dc645a78e325f10b0e4fe3e7b5ec23
Tiberiu Buzas <48926984+TiberiuBuzas@users.noreply.github.com>
2024-11-16 00:36:35 +0100

    Completed 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(pNum1, pNum2) {
+    let sum = 0;
 
+    if(!Number.isInteger(pNum1) || !Number.isInteger(pNum2)){
+        return "ERROR";
+    }
+    if(pNum1 < 0 || pNum2 < 0){
+        return "ERROR";
+    }
+
+    if (pNum1 > pNum2){ 
+        [pNum1, pNum2] = [pNum2, pNum1];
+    }
+    for (let index = pNum1; index <= pNum2; index++) {
+        sum += index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
