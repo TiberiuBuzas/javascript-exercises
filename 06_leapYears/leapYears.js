7a4e3b19f4151dfe9234e437152c826dde9f09aa
Tiberiu Buzas <48926984+TiberiuBuzas@users.noreply.github.com>
2024-11-16 00:59:31 +0100

    Completed 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(pYear) {
+    if(pYear % 100 == 0 && pYear % 400 == 0){
+        return true;
+    } else if(pYear % 100 == 0){
+        return false;
+    } else if(pYear % 4 == 0){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
