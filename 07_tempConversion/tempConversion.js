a055ef3e81d117feb8784994e11597a6269a55a4
Tiberiu Buzas <48926984+TiberiuBuzas@users.noreply.github.com>
2024-11-16 01:14:45 +0100

    Completed 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(pFahrenheit) {
+  let celsius = (5/9)*(pFahrenheit-32);
+  return Math.round(celsius*10)/10;;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(pCelsius) {
+  let fahrenheit = (9/5 * pCelsius) + 32;
+  return Math.round(fahrenheit*10)/10;;
 };
 
 // Do not edit below this line
