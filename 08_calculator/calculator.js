6c68a1fc941bc60914bcbd575ffb4b7081f13a5b
Tiberiu Buzas <48926984+TiberiuBuzas@users.noreply.github.com>
2024-11-27 00:03:51 +0100

    Completed 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(pNum1, pNum2) {
+	return pNum1 + pNum2;
 };
 
-const subtract = function() {
-	
+const subtract = function(pNum1, pNum2) {
+	return pNum1 - pNum2;
 };
 
-const sum = function() {
-	
+const sum = function(pArray) {
+	return pArray.reduce((total, currentValue) => {
+    return total + currentValue;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(pArray) {
+  let res = 1;
+  for (let index = 0; index < pArray.length; index++) {
+    res = res * pArray[index];
+  }
+  return res
 };
 
-const power = function() {
-	
+const power = function(pNum1, pNum2) {
+	return Math.pow(pNum1, pNum2);
 };
 
-const factorial = function() {
-	
+const factorial = function(pNum) {
+  let res = pNum;
+  if(pNum === 0 || pNum === 1) {
+    res = 1; 
+  }
+	for (let index = pNum-1; index > 0; index--) {
+    res *= index;
+  }
+  return res;
 };
 
 // Do not edit below this line
